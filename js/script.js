fef32850baaad8190b470b1b1945dddf5e2367b5
AndreaBevilacqua <andrebevi2000@gmail.com>
2024-01-22 16:38:27 +0100

    added gallery figures

--- js/script.js
+++ js/script.js
@@ -3,5 +3,9 @@ console.log('vue ok', Vue);
 const {createApp} = Vue;
 const app = createApp({
     name: 'VueCarousel',
-    data: () => ({ destinations })})
+    data: () => ({ 
+        destinations,
+        currentIndex: 0
+    })
+})
 app.mount('#root');
